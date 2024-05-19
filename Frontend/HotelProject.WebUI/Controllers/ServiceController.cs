3b56aed89c383ec7c09c37afb11a6b8d9eb8d497
emreu <emreu@DESKTOP-00UUE8M>
2024-05-19 03:59:35 +0300

    api update

--- Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
@@ -28,6 +29,73 @@ namespace HotelProject.WebUI.Controllers
                 return View(values);
             }
             return View();
+        }
+
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddService(CreateServiceDto model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");//Normal olan veriyi Json formatına dönüştürme işlemi
+            var responseMessaje = await client.PostAsync("http://localhost:12032/api/Services", stringContent);
+            if (responseMessaje.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:12032/api/Services/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:12032/api/Services/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDate = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(jsonDate);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");//Normal olan veriyi Json formatına dönüştürme işlemi
+            var responseMessaje = await client.PutAsync("http://localhost:12032/api/Services", stringContent);
+            if (responseMessaje.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
